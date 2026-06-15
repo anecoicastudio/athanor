88d92450391a48cf4f74281bd4c015b325dbb56c
marcoaccardi <contact.marco.accardi@gmail.com>
2026-06-15 14:02:09 +0200

    chore(m4-rsvp): gen:types for rsvps

--- packages/api/src/database.types.ts
+++ packages/api/src/database.types.ts
@@ -676,6 +676,48 @@ export type Database = {
           },
         ];
       };
+      rsvps: {
+        Row: {
+          created_at: string;
+          event_id: string;
+          id: string;
+          status: string;
+          updated_at: string;
+          user_id: string;
+        };
+        Insert: {
+          created_at?: string;
+          event_id: string;
+          id?: string;
+          status?: string;
+          updated_at?: string;
+          user_id: string;
+        };
+        Update: {
+          created_at?: string;
+          event_id?: string;
+          id?: string;
+          status?: string;
+          updated_at?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "rsvps_event_id_fkey";
+            columns: ["event_id"];
+            isOneToOne: false;
+            referencedRelation: "events";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "rsvps_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
       story_reactions: {
         Row: {
           created_at: string;
